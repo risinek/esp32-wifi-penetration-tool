ce653b1fbb8fd70798543c06837a6bb6523dfb15
risinek <risinek@gmail.com>
2021-01-31 19:54:31 +0100

    included esp idf logger library for debug logs

--- components/wifi_controller/wifi_controller.c
+++ components/wifi_controller/wifi_controller.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#include "esp_log.h"
+
+static const char* TAG = "wifi_controller";
+
 void wifi_start_ap(){
-    printf("starting wifi AP\n");
+    ESP_LOGD(TAG, "starting wifi AP");
 }
--- main/main.c
+++ main/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#include "esp_log.h"
+
 #include "wifi_controller.h"
+
+static const char* TAG = "main";
 
 void app_main(void)
 {
-    printf("app_main started\n");
+    ESP_LOGD(TAG, "app_main started");
     wifi_start_ap();
 }
