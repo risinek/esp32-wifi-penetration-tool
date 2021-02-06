ab2a0e4c65b163b6928ff59675c261673a1aed2f
risinek <risinek@gmail.com>
2021-02-06 10:31:57 +0100

    init webserver component

---
+++ components/webserver/webserver.c
@@ -0,0 +1,10 @@
+#include "webserver.h"
+
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#include "esp_log.h"
+
+static const char* TAG = "webserver";
+
+void webserver_run(){
+    ESP_LOGD(TAG, "Running webserver");
+}
---
+++ components/webserver/webserver.h
@@ -0,0 +1,6 @@
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+void webserver_run();
+
+#endif
