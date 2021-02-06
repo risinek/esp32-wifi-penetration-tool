4e262fbd20c0c9d1dab7ccb2da80684180c5090d
risinek <risinek@gmail.com>
2021-02-06 14:35:22 +0100

    separate page file to header file + changing endpoint name

---
+++ components/webserver/include/pages.h
@@ -0,0 +1,6 @@
+#ifndef PAGES_INDEX_H
+#define PAGES_INDEX_H
+
+const char page_root[] = "<h1>Attack configuration</h1>";
+
+#endif
--- components/webserver/webserver.c
+++ components/webserver/webserver.c
@@ -7,17 +7,18 @@
 #include "esp_err.h"
 #include "esp_http_server.h"
 
+#include "include/pages.h"
+
 static const char* TAG = "webserver";
 
-esp_err_t uri_get_handler(httpd_req_t *req) {
-    const char resp[] = "<h1>Test!</h1>";
-    return httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+esp_err_t uri_root_get_handler(httpd_req_t *req) {
+    return httpd_resp_send(req, page_root, HTTPD_RESP_USE_STRLEN);
 }
 
-httpd_uri_t uri_get = {
-    .uri = "/index",
+httpd_uri_t uri_root_get = {
+    .uri = "/",
     .method = HTTP_GET,
-    .handler = uri_get_handler,
+    .handler = uri_root_get_handler,
     .user_ctx = NULL
 };
 
@@ -28,6 +29,5 @@ void webserver_run(){
     httpd_handle_t server = NULL;
 
     ESP_ERROR_CHECK(httpd_start(&server, &config));
-    ESP_ERROR_CHECK(httpd_register_uri_handler(server, &uri_get));
-
+    ESP_ERROR_CHECK(httpd_register_uri_handler(server, &uri_root_get));
 }
