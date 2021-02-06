a284790e5bad31ce488817b0bf96086373cc34b3
risinek <risinek@gmail.com>
2021-02-06 22:23:19 +0100

    management webpage fetching aps endpoint via AJAX

--- components/webserver/include/pages.h
+++ components/webserver/include/pages.h
@@ -1,6 +1,28 @@
 #ifndef PAGES_INDEX_H
 #define PAGES_INDEX_H
 
-const char page_root[] = "<h1>Attack configuration</h1>";
+const char page_root[] = 
+"<!DOCTYPE html>"
+"<html>"
+    "<title>ESP32 Management AP</title>"
+"<body onLoad=\"refreshAps()\">"
+    "Available APs:"
+    "<div id=\"aps-list\">Loading...</div>"
+    "<button type=\"button\" onClick=\"refreshAps()\">Refresh</button>"
+    "<script>"
+    "function refreshAps() {"
+        "var xhttp = new XMLHttpRequest();"
+        "xhttp.onreadystatechange = function() {"
+            "if(this.readyState == 4 && this.status == 200) {"
+                "document.getElementById(\"aps-list\").innerHTML = this.responseText;"
+            "}"
+        "};"
+        "xhttp.open(\"GET\", \"aps\", true);"
+        "xhttp.send();"
+    "}"
+    "</script>"
+"</body>"
+"</html>"
+;
 
 #endif
