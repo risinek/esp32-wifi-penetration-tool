a5846880498646e49e45aff9affcb9da0fb56f34
risinek <risinek@gmail.com>
2021-02-27 21:55:55 +0100

    data frames handling from event loop posted by sniffer

--- components/frame_analyzer/frame_analyzer.c
+++ components/frame_analyzer/frame_analyzer.c
@@ -3,8 +3,18 @@
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
 #include "esp_err.h"
+#include "esp_event.h"
 
-static const char *TAG = "frame_analyzer"; 
+#include "wifi_controller.h"
+
+static const char *TAG = "frame_analyzer";
+
+static void data_frame_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    ESP_LOGD(TAG, "Handling DATA frame");
+}
+
 void frame_analyzer_capture_wpa_handshake(){
     ESP_LOGI(TAG, "Capturing WPA handshake frames...");
+
+    ESP_ERROR_CHECK(esp_event_handler_register(SNIFFER_EVENTS, SNIFFER_EVENT_CAPTURED_DATA, &data_frame_handler, NULL));
 }
