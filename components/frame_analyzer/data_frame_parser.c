c4f5d4667b717774a4d41c6e07101d9fb16747b3
risinek <risinek@gmail.com>
2021-02-28 00:32:58 +0100

    eapol packet struct types

--- components/frame_analyzer/data_frame_parser.c
+++ components/frame_analyzer/data_frame_parser.c
@@ -22,6 +22,25 @@ typedef struct {
     uint8_t payload[0];
 } data_frame_t;
 
+typedef struct {
+	uint8_t version;
+	uint8_t packet_type;
+	uint16_t packet_body_length;
+} eapol_header_t;
+
+typedef struct {
+	eapol_header_t hdr;
+	uint8_t payload[];
+} eapol_packet_t;
+
+typedef enum {
+    EAPOL_EAP_PACKET,
+	EAPOL_START,
+	EAPOL_LOGOFF,
+	EAPOL_KEY,
+	EAPOL_ENCAPSULATED_ASF_ALERT
+} eapol_packet_types_t;
+
 typedef enum {
 	ETHER_TYPE_EAPOL = 0x888e
 } ether_types_t;
