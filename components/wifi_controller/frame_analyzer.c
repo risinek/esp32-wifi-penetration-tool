e010ac9b766e84dd663e9e18efc271f0bc79e638
risinek <risinek@gmail.com>
2021-02-27 11:58:33 +0100

    basic promiscuous mode

--- components/wifi_controller/frame_analyzer.c
+++ components/wifi_controller/frame_analyzer.c
@@ -3,9 +3,17 @@
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
 #include "esp_err.h"
+#include "esp_wifi.h"
+#include "esp_wifi_types.h"
 
 static const char *TAG = "frame_analyzer"; 
 
+static void frame_handler(void *buf, wifi_promiscuous_pkt_type_t type) {
+    ESP_LOGD(TAG, "Captured frame.");
+}
+
 void wifictl_frame_analyzer_start() {
-    ESP_LOGI(TAG, "Initialising frame_analyzer component...");
+    ESP_LOGI(TAG, "Initialising frame analyzer...");
+    esp_wifi_set_promiscuous(true);
+    esp_wifi_set_promiscuous_rx_cb(&frame_handler);
 }
