8370dd40afb37a20dc1756fc4997c5fa98fa5a94
risinek <risinek@gmail.com>
2021-02-28 00:25:43 +0100

    prepare logging

--- components/frame_analyzer/data_frame_parser.c
+++ components/frame_analyzer/data_frame_parser.c
@@ -1,3 +1,7 @@
 #include "data_frame_parser.h"
+
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#include "esp_log.h"
+const char *TAG = "frame_analyzer:data_frame_parser";
 void parse_data_frame(void *frame) {
 }
