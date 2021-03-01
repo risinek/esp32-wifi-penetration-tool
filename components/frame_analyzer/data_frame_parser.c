8f6a7bc089735645d227a763c36d85e28d2725a3
risinek <risinek@gmail.com>
2021-03-01 23:30:01 +0100

    WIP: parsing data frame up to EAPOL header (enough for WPA handshake capture)

--- components/frame_analyzer/data_frame_parser.c
+++ components/frame_analyzer/data_frame_parser.c
@@ -21,6 +21,44 @@ void print_mac_address(uint8_t *a){
 }
 
 void parse_data_frame(void *frame) {
-    wifi_promiscuous_pkt_t *pframe = (wifi_promiscuous_pkt_t *) frame;
-    print_raw_frame(pframe);
+    uint8_t *frame_buffer = ((wifi_promiscuous_pkt_t *) frame)->payload;
+
+    data_frame_mac_header_t *mac_header = (data_frame_mac_header_t *) frame_buffer;
+    frame_buffer += sizeof(data_frame_mac_header_t);
+
+
+    if((mac_header->addr1[0] != 0x04) && (mac_header->addr2[0] != 0x04)) {
+        return;
+    }
+
+    if(mac_header->frame_control.protected_frame == 1) {
+        ESP_LOGV(TAG, "Protected frame, skipping...");
+        return;
+    }
+
+    if(mac_header->frame_control.subtype > 7) {
+        ESP_LOGD(TAG, "QoS data frame");
+        // Skipping QoS field (2 bytes)
+        frame_buffer += 2;
+    }
+
+    // Skipping LLC SNAP header (6 bytes)
+    frame_buffer += sizeof(llc_snap_header_t);
+
+    print_mac_address(mac_header->addr1);
+    printf("; ");
+    print_mac_address(mac_header->addr2);
+    printf("\n");
+    print_raw_frame(frame);
+
+    // Check if frame_data is type of EAPoL
+    if(ntohs(*(uint16_t *) frame_buffer) == ETHER_TYPE_EAPOL) {
+        frame_buffer += 2;
+        ESP_LOGD(TAG, "EAPOL packet");
+        eapol_packet_header_t *eapol_packet_header = (eapol_packet_header_t *) frame_buffer;
+        frame_buffer += sizeof(eapol_packet_header_t);
+        if(eapol_packet_header->packet_type == EAPOL_KEY) {
+            ESP_LOGD(TAG, "EAPOL-Key");
+        }
+    }
 }
