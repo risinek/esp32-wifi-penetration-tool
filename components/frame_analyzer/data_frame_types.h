c9525c554613f06bdeb0cd33181921dab0037fb6
risinek <risinek@gmail.com>
2021-03-01 23:21:39 +0100

    added frame control structure

--- components/frame_analyzer/data_frame_types.h
+++ components/frame_analyzer/data_frame_types.h
@@ -15,7 +15,21 @@ typedef enum {
 } eapol_packet_types_t;
 
 typedef struct {
-    uint16_t frame_ctrl;
+    uint8_t protocol_version:2;
+    uint8_t type:2;
+    uint8_t subtype:4;
+    uint8_t to_ds:1;
+    uint8_t from_ds:1;
+    uint8_t more_fragments:1;
+    uint8_t retry:1;
+    uint8_t power_management:1;
+    uint8_t more_data:1;
+    uint8_t protected_frame:1;
+    uint8_t htc_order:1;
+} frame_control_t;
+
+typedef struct {
+    frame_control_t frame_control;
     uint16_t duration;
     uint8_t addr1[6];
     uint8_t addr2[6];
