b2f6bd11e47b6c45a715bf64c1ebbb87766cc27e
risinek <risinek@gmail.com>
2021-03-05 21:17:16 +0100

    fix wrong variable assignment + renaming parameter arg->args

--- components/frame_analyzer/frame_analyzer.c
+++ components/frame_analyzer/frame_analyzer.c
@@ -10,10 +10,10 @@
 
 static const char *TAG = "frame_analyzer";
 
-static void data_frame_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+static void data_frame_handler(void *args, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     ESP_LOGV(TAG, "Handling DATA frame");
-    wifi_promiscuous_pkt_t *frame = (wifi_promiscuous_pkt_t *) frame;
-    if(parse_eapol_packet(event_data) != NULL){
+    wifi_promiscuous_pkt_t *frame = (wifi_promiscuous_pkt_t *) event_data;
+    if(parse_eapol_packet(frame) != NULL){
         ESP_ERROR_CHECK(esp_event_post(DATA_FRAME_EVENTS, DATA_FRAME_EVENT_CAPTURED_EAPOLKEY, frame->payload, frame->rx_ctrl.sig_len, portMAX_DELAY));
     }
 }
