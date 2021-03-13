966fd8f40fb02e4a3ad6fe5c804ef62d4db22c0d
risinek <risinek@gmail.com>
2021-03-13 20:51:39 +0100

    introduce attack timeout logic

--- main/attack.c
+++ main/attack.c
@@ -1,5 +1,6 @@
 #include "attack.h"
 
+#include <unistd.h>
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
 #include "esp_err.h"
@@ -8,7 +9,6 @@
 
 static const char* TAG = "attack";
 static attack_result_t attack_result = { .status = IDLE };
-// static attack_config_t attack_config = { .ap_record = NULL, .timeout  = 0, .type = PASSIVE};
 
 const attack_result_t *attack_get_result() {
     return &attack_result;
@@ -16,6 +16,15 @@ const attack_result_t *attack_get_result() {
 
 void attack_set_result(attack_status_t status) {
     attack_result.status = status;
+}
+
+static bool attack_timeout(unsigned seconds){
+    sleep(seconds);
+    if(attack_result.status == FINISHED) {
+        ESP_LOGD(TAG, "Attack already finished. Not doing anything...");
+        return false;
+    }
+    return true;
 }
 
 void attack_run(attack_config_t attack_config) {
@@ -28,7 +37,12 @@ void attack_run(attack_config_t attack_config) {
     }
     switch(attack_config.type) {
         case ATTACK_TYPE_PMKID:
-            attack_pmkid(&attack_config);
+            attack_pmkid_start(&attack_config);
+            if(attack_timeout(5)){
+                ESP_LOGD(TAG, "Aborting attack!");
+                attack_set_result(TIMEOUT);
+                attack_pmkid_stop();
+            }
             break;
         case ATTACK_TYPE_HANDSHAKE:
             ESP_LOGI(TAG, "Attack on WPA handshake...");
