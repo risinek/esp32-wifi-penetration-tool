bdde1bddbbb70d0692243466e4ac56bd003ec9d5
risinek <risinek@gmail.com>
2021-03-14 16:47:57 +0100

    fix data size when sending pointer to pmkid_item_t to event loop

--- components/frame_analyzer/frame_analyzer.c
+++ components/frame_analyzer/frame_analyzer.c
@@ -27,7 +27,7 @@ static void data_frame_handler(void *args, esp_event_base_t event_base, int32_t 
         ESP_ERROR_CHECK(esp_event_post(DATA_FRAME_EVENTS, DATA_FRAME_EVENT_CAPTURED_EAPOLKEY, frame->payload, frame->rx_ctrl.sig_len, portMAX_DELAY));
         pmkid_item_t *pmkid_items;
         if((pmkid_items = parse_pmkid_from_eapol_packet(eapol_packet)) != NULL){
-            ESP_ERROR_CHECK(esp_event_post(DATA_FRAME_EVENTS, DATA_FRAME_EVENT_FOUND_PMKID, pmkid_items, sizeof(pmkid_item_t), portMAX_DELAY));
+            ESP_ERROR_CHECK(esp_event_post(DATA_FRAME_EVENTS, DATA_FRAME_EVENT_FOUND_PMKID, &pmkid_items, sizeof(pmkid_item_t *), portMAX_DELAY));
         }
     }
 }
