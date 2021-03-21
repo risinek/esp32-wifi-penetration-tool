f3f1a5fb919c7b6f6134ffbab73ecea5643a08a5
risinek <risinek@gmail.com>
2021-03-21 17:41:08 +0100

    reoreder file

--- components/deauther/deauther.c
+++ components/deauther/deauther.c
@@ -9,10 +9,6 @@
 #include "esp_wifi.h"
 #include "esp_wifi_types.h"
 
-int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3) {
-  return 0;
-}
-
 static const char *TAG = "deauther";
 static const uint8_t deauth_frame_default[] = {
     0xc0, 0x00, 0x3a, 0x01,
@@ -21,6 +17,10 @@ static const uint8_t deauth_frame_default[] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x01, 0x00
 };
+
+int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3){
+    return 0;
+}
 
 void deauther_send_deauth_frame(){
     ESP_LOGD(TAG, "Sending deauth frame...");
