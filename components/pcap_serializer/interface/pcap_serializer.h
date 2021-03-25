cb5dea4bd0eeed1a9558590f59168c1d197d24d0
risinek <risinek@gmail.com>
2021-03-25 15:57:24 +0100

    pcap serialization functions (variation on already implemented attack statuc content appending)

--- components/pcap_serializer/interface/pcap_serializer.h
+++ components/pcap_serializer/interface/pcap_serializer.h
@@ -1,9 +1,9 @@
 #ifndef PCAP_SERIALIZER_H
 #define PCAP_SERIALIZER_H
 
-#include <unistd.h>
+#include <stdint.h>
 
-// Ref: https://gitlab.com/wireshark/wireshark/-/wikis/Development/LibpcapFileFormat
+// Ref: https://gitlab.com/wireshark/wireshark/-/wikis/Development/LibpcapFileFormat#global-header
 typedef struct {
             uint32_t magic_number;   /* magic number */
             uint16_t version_major;  /* major version number */
@@ -21,5 +21,11 @@ typedef struct {
         uint32_t incl_len;       /* number of octets of packet saved in file */
         uint32_t orig_len;       /* actual length of packet */
 } pcap_record_header_t;
+
+uint8_t *pcap_serializer_init_new();
+void pcap_serializer_append_frame(const uint8_t *buffer, unsigned size, unsigned ts_usec);
+void pcap_serializer_deinit();
+unsigned pcap_serializer_get_size();
+uint8_t *pcap_serializer_get_buffer();
 
 #endif
