5060194abc8ef7d1a6f6d55e064b8268f105801e
risinek <risinek@gmail.com>
2021-04-03 01:00:16 +0200

    document attack

--- main/attack.c
+++ main/attack.c
@@ -1,3 +1,12 @@
+/**
+ * @file attack.c
+ * @author risinek (risinek@gmail.com)
+ * @date 2021-04-02
+ * @copyright Copyright (c) 2021
+ * 
+ * @brief Implementation of attack wrapper
+ */
+
 #include "attack.h"
 
 #include <stdlib.h>
@@ -25,7 +34,6 @@ const attack_status_t *attack_get_status() {
 void attack_update_status(attack_state_t state) {
     attack_status.state = state;
     if(state == FINISHED) {
-        // Stop timeout timer
         ESP_LOGD(TAG, "Stopping attack timeout timer");
         ESP_ERROR_CHECK(esp_timer_stop(attack_timeout_handle));
     } 
@@ -52,6 +60,14 @@ char *attack_alloc_result_content(unsigned size) {
     return attack_status.content;
 }
 
+/**
+ * @brief Callback function for attack timeout timer.
+ * 
+ * This function is called when attack times out. 
+ * It updates attack status state to TIMEOUT.
+ * It calls appropriate abort functions based on current attack type.
+ * @param arg not used.
+ */
 static void attack_timeout(void* arg){
     ESP_LOGD(TAG, "Attack timed out");
     
@@ -74,6 +90,20 @@ static void attack_timeout(void* arg){
     }
 }
 
+/**
+ * @brief Callback for WEBSERVER_EVENT_ATTACK_REQUEST event.
+ * 
+ * This function handles WEBSERVER_EVENT_ATTACK_REQUEST event from event loop.
+ * It parses attack_request_t structure and set initial values to attack_status.
+ * It sets attack state to RUNNING.
+ * It starts attack timeout timer.
+ * It starts attack based on chosen type.
+ * 
+ * @param args not used
+ * @param event_base expects WEBSERVER_EVENTS
+ * @param event_id expects WEBSERVER_EVENT_ATTACK_REQUEST
+ * @param event_data expects attack_request_t
+ */
 static void attack_request_handler(void *args, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     ESP_LOGI(TAG, "Starting attack...");
     attack_request_t *attack_request = (attack_request_t *) event_data;
@@ -105,6 +135,16 @@ static void attack_request_handler(void *args, esp_event_base_t event_base, int3
     }
 }
 
+/**
+ * @brief Callback for WEBSERVER_EVENT_ATTACK_RESET event.
+ * 
+ * This callback resets attack status by freeing previously allocated status content and putting attack to READY state.
+ * 
+ * @param args not used
+ * @param event_base expects WEBSERVER_EVENTS
+ * @param event_id expects WEBSERVER_EVENT_ATTACK_RESET
+ * @param event_data not used
+ */
 static void attack_reset_handler(void *args, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     ESP_LOGD(TAG, "Resetting attack status...");
     if(attack_status.content){
@@ -116,6 +156,12 @@ static void attack_reset_handler(void *args, esp_event_base_t event_base, int32_
     attack_status.state = READY;
 }
 
+/**
+ * @brief Initialises common attack resources.
+ * 
+ * Creates attack timeout timer.
+ * Registers event loop event handlers.
+ */
 void attack_init(){
     const esp_timer_create_args_t attack_timeout_args = {
         .callback = &attack_timeout
