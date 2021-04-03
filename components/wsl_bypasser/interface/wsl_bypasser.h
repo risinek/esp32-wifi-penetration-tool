837164a582affd37355e42dc37176f414174485d
risinek <risinek@gmail.com>
2021-04-03 19:45:57 +0200

    make WSL Bypasser universal - allow any raw frame buffer to be send

--- components/wsl_bypasser/interface/wsl_bypasser.h
+++ components/wsl_bypasser/interface/wsl_bypasser.h
@@ -3,6 +3,7 @@
 
 #include "esp_wifi_types.h"
 
+void wsl_bypasser_send_raw_frame(const uint8_t *frame_buffer, int size);
 void wsl_bypasser_send_deauth_frame(const wifi_ap_record_t *ap_record);
 
 #endif
--- components/wsl_bypasser/wsl_bypasser.c
+++ components/wsl_bypasser/wsl_bypasser.c
@@ -24,6 +24,10 @@ int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3){
     return 0;
 }
 
+void wsl_bypasser_send_raw_frame(const uint8_t *frame_buffer, int size){
+    ESP_ERROR_CHECK(esp_wifi_80211_tx(WIFI_IF_AP, frame_buffer, size, false));
+}
+
 void wsl_bypasser_send_deauth_frame(const wifi_ap_record_t *ap_record){
     ESP_LOGD(TAG, "Sending deauth frame...");
     uint8_t deauth_frame[sizeof(deauth_frame_default)];
@@ -31,5 +35,5 @@ void wsl_bypasser_send_deauth_frame(const wifi_ap_record_t *ap_record){
     memcpy(&deauth_frame[10], ap_record->bssid, 6);
     memcpy(&deauth_frame[16], ap_record->bssid, 6);
     
-    ESP_ERROR_CHECK(esp_wifi_80211_tx(WIFI_IF_AP, deauth_frame, sizeof(deauth_frame), false));
+    wsl_bypasser_send_raw_frame(deauth_frame, sizeof(deauth_frame_default));
 }
