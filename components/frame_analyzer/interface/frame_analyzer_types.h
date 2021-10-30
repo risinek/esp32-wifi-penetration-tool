e8cfc291e45e7d0cc69645a37e17e1baf02f8139
skbeh <60107333+skbeh@users.noreply.github.com>
2021-10-30 18:01:22 +0800

    Use random password for dummy ap and refresh style

--- components/frame_analyzer/interface/frame_analyzer_types.h
+++ components/frame_analyzer/interface/frame_analyzer_types.h
@@ -3,8 +3,9 @@
  * @author risinek (risinek@gmail.com)
  * @date 2021-04-05
  * @copyright Copyright (c) 2021
- * 
- * @brief Provides structures and constants based on various standards like 802.11, 802.1X etc...
+ *
+ * @brief Provides structures and constants based on various standards like
+ * 802.11, 802.1X etc...
  */
 #ifndef FRAME_ANALYZER_TYPES_H
 #define FRAME_ANALYZER_TYPES_H
@@ -20,50 +21,50 @@
  * @see Ref: 802.1X-2020 [11.3.2]
  */
 typedef enum {
-    EAPOL_EAP_PACKET = 0,
-	EAPOL_START,
-	EAPOL_LOGOFF,
-	EAPOL_KEY,
-	EAPOL_ENCAPSULATED_ASF_ALERT,
-    EAPOL_MKA,
-    EAPOL_ANNOUNCEMENT_GENERIC,
-    EAPOL_ANNOUNCEMENT_SPECIFIC,
-    EAPOL_ANNOUNCEMENT_REQ
+  EAPOL_EAP_PACKET = 0,
+  EAPOL_START,
+  EAPOL_LOGOFF,
+  EAPOL_KEY,
+  EAPOL_ENCAPSULATED_ASF_ALERT,
+  EAPOL_MKA,
+  EAPOL_ANNOUNCEMENT_GENERIC,
+  EAPOL_ANNOUNCEMENT_SPECIFIC,
+  EAPOL_ANNOUNCEMENT_REQ
 } eapol_packet_types_t;
 
 typedef struct {
-    uint8_t protocol_version:2;
-    uint8_t type:2;
-    uint8_t subtype:4;
-    uint8_t to_ds:1;
-    uint8_t from_ds:1;
-    uint8_t more_fragments:1;
-    uint8_t retry:1;
-    uint8_t power_management:1;
-    uint8_t more_data:1;
-    uint8_t protected_frame:1;
-    uint8_t htc_order:1;
+  uint8_t protocol_version : 2;
+  uint8_t type : 2;
+  uint8_t subtype : 4;
+  uint8_t to_ds : 1;
+  uint8_t from_ds : 1;
+  uint8_t more_fragments : 1;
+  uint8_t retry : 1;
+  uint8_t power_management : 1;
+  uint8_t more_data : 1;
+  uint8_t protected_frame : 1;
+  uint8_t htc_order : 1;
 } frame_control_t;
 
 typedef struct {
-    frame_control_t frame_control;
-    uint16_t duration;
-    uint8_t addr1[6];
-    uint8_t addr2[6];
-    uint8_t addr3[6];
-    uint16_t sequence_control;
+  frame_control_t frame_control;
+  uint16_t duration;
+  uint8_t addr1[6];
+  uint8_t addr2[6];
+  uint8_t addr3[6];
+  uint16_t sequence_control;
 } data_frame_mac_header_t;
 
 typedef struct {
-    data_frame_mac_header_t mac_header;
-    uint8_t body[];
+  data_frame_mac_header_t mac_header;
+  uint8_t body[];
 } data_frame_t;
 
 typedef struct {
-    uint8_t snap_dsap;
-    uint8_t snap_ssap;
-    uint8_t control;
-    uint8_t encapsulation[3];
+  uint8_t snap_dsap;
+  uint8_t snap_ssap;
+  uint8_t control;
+  uint8_t encapsulation[3];
 } llc_snap_header_t;
 
 /**
@@ -71,17 +72,17 @@ typedef struct {
  * @see Ref: 802.1X-2020 [11.3]
  */
 typedef struct {
-	uint8_t version;
-	uint8_t packet_type;
-	uint16_t packet_body_length;
+  uint8_t version;
+  uint8_t packet_type;
+  uint16_t packet_body_length;
 } eapol_packet_header_t;
 
 /**
  * @see Ref: 802.1X-2020 [11.3], 802.11-2016 [12.7.2]
  */
 typedef struct {
-	eapol_packet_header_t header;
-	uint8_t packet_body[];
+  eapol_packet_header_t header;
+  uint8_t packet_body[];
 } eapol_packet_t;
 
 /**
@@ -90,36 +91,37 @@ typedef struct {
  * @see Ref: 802.11-2016 [12.7.2]
  */
 typedef struct {
-    uint8_t key_descriptor_version:3;
-    uint8_t key_type:1;
-    uint8_t :2;
-    uint8_t install:1;
-    uint8_t key_ack:1;
-    uint8_t key_mic:1;
-    uint8_t secure:1;
-    uint8_t error:1;
-    uint8_t request:1;
-    uint8_t encrypted_key_data:1;
-    uint8_t smk_message:1;
-    uint8_t :2;
+  uint8_t key_descriptor_version : 3;
+  uint8_t key_type : 1;
+  uint8_t : 2;
+  uint8_t install : 1;
+  uint8_t key_ack : 1;
+  uint8_t key_mic : 1;
+  uint8_t secure : 1;
+  uint8_t error : 1;
+  uint8_t request : 1;
+  uint8_t encrypted_key_data : 1;
+  uint8_t smk_message : 1;
+  uint8_t : 2;
 } key_information_t;
 
 /**
- * @todo MIC lenght is dependent on 802.11-2016 [12.7.3] - key_infromation_t.descrptor_version
+ * @todo MIC lenght is dependent on 802.11-2016 [12.7.3] -
+ * key_infromation_t.descrptor_version
  * @see Ref: 802.11-2016 [12.7.2]
  */
 typedef struct __attribute__((__packed__)) {
-    uint8_t descriptor_type;
-    key_information_t key_information;
-    uint16_t key_length;
-    uint8_t key_replay_counter[8];
-    uint8_t key_nonce[32];
-    uint8_t key_iv[16];
-    uint8_t key_rsc[8];
-    uint8_t reserved[8];
-    uint8_t key_mic[16];
-    uint16_t key_data_length;
-    uint8_t key_data[];
+  uint8_t descriptor_type;
+  key_information_t key_information;
+  uint16_t key_length;
+  uint8_t key_replay_counter[8];
+  uint8_t key_nonce[32];
+  uint8_t key_iv[16];
+  uint8_t key_rsc[8];
+  uint8_t reserved[8];
+  uint8_t key_mic[16];
+  uint16_t key_data_length;
+  uint8_t key_data[];
 } eapol_key_packet_t;
 
 /**
@@ -128,7 +130,8 @@ typedef struct __attribute__((__packed__)) {
 #define KEY_DATA_TYPE 0xdd
 
 /**
- * @note Needs trailing byte due to casting to uint32_t and converting from netlong
+ * @note Needs trailing byte due to casting to uint32_t and converting from
+ * netlong
  * @see Ref: 802.11-2016 [12.7.2, Table 12-6]
  */
 #define KEY_DATA_OUI_IEEE80211 0x00fac00
@@ -142,19 +145,19 @@ typedef struct __attribute__((__packed__)) {
  * @see Ref: 802.11-2016 [12.7.2]
  */
 typedef struct __attribute__((__packed__)) {
-    uint8_t type;
-    uint8_t length;
-    uint32_t oui:24;
-    uint32_t data_type:8;
-    uint8_t data[];
+  uint8_t type;
+  uint8_t length;
+  uint32_t oui : 24;
+  uint32_t data_type : 8;
+  uint8_t data[];
 } key_data_field_t;
 
 /**
  * @brief linked list of PMKIDs
  */
 typedef struct pmkid_item {
-    uint8_t pmkid[16];
-    struct pmkid_item *next;
+  uint8_t pmkid[16];
+  struct pmkid_item *next;
 } pmkid_item_t;
 
 #endif
--- components/pcap_serializer/interface/pcap_serializer.h
+++ components/pcap_serializer/interface/pcap_serializer.h
@@ -3,8 +3,9 @@
  * @author risinek (risinek@gmail.com)
  * @date 2021-04-05
  * @copyright Copyright (c) 2021
- * 
- * @brief Provides interface to generate PCAP formatted binary from raw frame bytes 
+ *
+ * @brief Provides interface to generate PCAP formatted binary from raw frame
+ * bytes
  */
 #ifndef PCAP_SERIALIZER_H
 #define PCAP_SERIALIZER_H
@@ -13,34 +14,36 @@
 
 /**
  * @brief PCAP global header
- * 
- * @see Ref: https://gitlab.com/wireshark/wireshark/-/wikis/Development/LibpcapFileFormat#global-header
+ *
+ * @see Ref:
+ * https://gitlab.com/wireshark/wireshark/-/wikis/Development/LibpcapFileFormat#global-header
  */
 typedef struct {
-            uint32_t magic_number;   /* magic number */
-            uint16_t version_major;  /* major version number */
-            uint16_t version_minor;  /* minor version number */
-            int32_t  thiszone;       /* GMT to local correction */
-            uint32_t sigfigs;        /* accuracy of timestamps */
-            uint32_t snaplen;        /* max length of captured packets, in octets */
-            uint32_t network;        /* data link type */
+  uint32_t magic_number;  /* magic number */
+  uint16_t version_major; /* major version number */
+  uint16_t version_minor; /* minor version number */
+  int32_t thiszone;       /* GMT to local correction */
+  uint32_t sigfigs;       /* accuracy of timestamps */
+  uint32_t snaplen;       /* max length of captured packets, in octets */
+  uint32_t network;       /* data link type */
 } pcap_global_header_t;
 
 /**
  * @brief PCAP record header
- * 
- * @see Ref: https://gitlab.com/wireshark/wireshark/-/wikis/Development/LibpcapFileFormat
+ *
+ * @see Ref:
+ * https://gitlab.com/wireshark/wireshark/-/wikis/Development/LibpcapFileFormat
  */
 typedef struct {
-        uint32_t ts_sec;         /* timestamp seconds */
-        uint32_t ts_usec;        /* timestamp microseconds */
-        uint32_t incl_len;       /* number of octets of packet saved in file */
-        uint32_t orig_len;       /* actual length of packet */
+  uint32_t ts_sec;   /* timestamp seconds */
+  uint32_t ts_usec;  /* timestamp microseconds */
+  uint32_t incl_len; /* number of octets of packet saved in file */
+  uint32_t orig_len; /* actual length of packet */
 } pcap_record_header_t;
 
 /**
- * @brief Prepares new empty buffer for PCAP formatted binary data. 
- * 
+ * @brief Prepares new empty buffer for PCAP formatted binary data.
+ *
  * Has always to be called before pcap_serializer_append_frame()
  * @return uint8_t* pointer to newly allocated PCAP buffer.
  * @return \c NULL initialisation failed
@@ -49,33 +52,35 @@ uint8_t *pcap_serializer_init();
 
 /**
  * @brief Appends new frame to existing PCAP buffer.
- * 
+ *
  * Expects pcap_serializer_append_frame() was already called.
  * @param buffer frame buffer that should be appended to PCAP
  * @param size size of frame buffer
  * @param ts_usec timestamp of captured frame in microseconds
  */
-void pcap_serializer_append_frame(const uint8_t *buffer, unsigned size, unsigned ts_usec);
+void pcap_serializer_append_frame(const uint8_t *buffer, unsigned size,
+                                  unsigned ts_usec);
 
 /**
  * @brief Frees PCAP buffer and resets all values.
- * 
- * After calling this function, you have to call pcap_serializer_init() to append new frames again.
- * 
+ *
+ * After calling this function, you have to call pcap_serializer_init() to
+ * append new frames again.
+ *
  */
 void pcap_serializer_deinit();
 
 /**
  * @brief Returns size of PCAP buffer in bytes
- * 
+ *
  * @return unsigned
  */
 unsigned pcap_serializer_get_size();
 
 /**
  * @brief Return pointer to PCAP buffer
- * 
- * @return uint8_t* 
+ *
+ * @return uint8_t*
  */
 uint8_t *pcap_serializer_get_buffer();
 
